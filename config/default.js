7483b9cee7637a6a789ecbf4a32971ece05e4ef7
gondzo <gondzo@gmail.com>
2017-12-15 19:03:36 +0100

    change default port

--- config/default.js
+++ config/default.js
@@ -7,7 +7,7 @@ module.exports = {
   ALLOWED_SERVICES: process.env.ALLOWED_SERVICES ? process.env.ALLOWED_SERVICES.replace(/\\"/g, '').split(',') : ['project-service'],
   LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
   CONTEXT_PATH: process.env.API_VERSION || '/eventbus/',
-  PORT: process.env.PORT || '4200',
+  PORT: process.env.PORT || '3000',
   JWT_TOKEN_SECRET: process.env.JWT_TOKEN_SECRET || 'JWT_TOKEN_SECRET',
   JWT_TOKEN_EXPIRES_IN: process.env.JWT_TOKEN_EXPIRES_IN || '100 days',
   KAFKA_TOPIC_PREFIX: process.env.KAFKA_TOPIC_PREFIX || 'joan-26673.notifications.'
