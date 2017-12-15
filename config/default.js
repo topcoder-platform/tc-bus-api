001b1af0a2dce1b627654d10e44a1c482bb5c02b
Samir Gondzetovic <gondzo@gmail.com>
2017-12-15 17:06:02 +0000

    new events integration

--- config/default.js
+++ config/default.js
@@ -4,12 +4,11 @@
 module.exports = {
     KAFKA_CLIENT_CERT: process.env.KAFKA_CLIENT_CERT ? process.env.KAFKA_CLIENT_CERT.replace('\\n', '\n') : null,
   KAFKA_CLIENT_CERT_KEY: process.env.KAFKA_CLIENT_CERT_KEY ? process.env.KAFKA_CLIENT_CERT_KEY.replace('\\n', '\n') : null,
-  ALLOWED_SERVICES: process.env.ALLOWED_SERVICES ? process.env.ALLOWED_SERVICES.replace(/\\"/g, '') : null,
-  LOG_LEVEL: process.env.LOG_LEVEL,
-  CONTEXT_PATH: process.env.API_VERSION,
-  PORT: process.env.PORT,
-  ALLOWED_SERVICES: process.env.ALLOWED_SERVICES,
-  JWT_TOKEN_SECRET: process.env.JWT_TOKEN_SECRET,
-  JWT_TOKEN_EXPIRES_IN: process.env.JWT_TOKEN_EXPIRES_IN,
-  KAFKA_TOPIC_PREFIX: process.env.KAFKA_TOPIC_PREFIX
+  ALLOWED_SERVICES: process.env.ALLOWED_SERVICES ? process.env.ALLOWED_SERVICES.replace(/\\"/g, '').split(',') : ['project-service'],
+  LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
+  CONTEXT_PATH: process.env.API_VERSION || '/eventbus/',
+  PORT: process.env.PORT || '4200',
+  JWT_TOKEN_SECRET: process.env.JWT_TOKEN_SECRET || 'JWT_TOKEN_SECRET',
+  JWT_TOKEN_EXPIRES_IN: process.env.JWT_TOKEN_EXPIRES_IN || '100 days',
+  KAFKA_TOPIC_PREFIX: process.env.KAFKA_TOPIC_PREFIX || 'joan-26673.notifications.'
 }
