059620b091e4add21d6c634d61e674b4fc205fe0
Unknown <mtwomey@beakstar.com>
2017-12-22 19:30:20 -0600

    Update health check

--- controllers/HealthController.js
+++ controllers/HealthController.js
@@ -8,9 +8,9 @@
  * @param req the request
  * @param res the response
  */
-function health(req, res, next) {
-  res.json({health: 'ok'});
-  next();
+async function health(req, res, next) {
+  res.json({health: 'ok'})
+  next()
 }
 
 // Exports
