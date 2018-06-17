207763cd88029165d7f922c3d40fe97433c2f44f
Mithun Kamath <callmekatootie@gmail.com>
2018-06-17 21:58:54 +0530

    Copy legacy code to v5 (wherever applicable)

---
+++ common/helper.js
@@ -0,0 +1,207 @@
+/**
+ * This file defines helper methods.
+ */
+const util = require('util')
+const _ = require('lodash')
+const Joi = require('joi')
+const getParams = require('get-parameter-names')
+const config = require('config')
+const jwt = require('jsonwebtoken')
+const createError = require('http-errors')
+
+const logger = require('./logger')
+
+/**
+ * Convert array with arguments to object.
+ *
+ * @param {Array} params the name of parameters
+ * @param {Array} arr the array with values
+ * @returns {Object} the combined object
+ * @private
+ */
+function combineObject (params, arr) {
+  const ret = {}
+  _.forEach(arr, (arg, i) => {
+    ret[params[i]] = arg
+  })
+  return ret
+}
+
+/**
+ * Decorate all functions of a service and log debug information if DEBUG is enabled.
+ *
+ * @param {Object} service the service
+ * @private
+ */
+function decorateWithLogging (service) {
+  if (config.LOG_LEVEL !== 'debug') {
+    return
+  }
+  _.forEach(service, (method, name) => {
+    const params = method.params || getParams(method)
+    service[name] = async function serviceMethodWithLogging (...args) {
+      logger.debug(`ENTER ${name}`)
+      logger.debug('input arguments')
+      logger.debug(util.inspect(combineObject(params, args)))
+      const result = await method.apply(this, args)
+      logger.debug(`EXIT ${name}`)
+      logger.debug('output arguments')
+      logger.debug(util.inspect(result))
+      return result
+    }
+  })
+}
+
+/**
+ * Decorate all functions of a service and validate input values
+ * and replace input arguments with sanitized result = require('Joi.
+ * Service method must have a `schema` property with Joi schema.
+ *
+ * @param {Object} service the service
+ * @private
+ */
+function decorateWithValidator (service) {
+  _.forEach(service, (method, name) => {
+    if (!method.schema) {
+      return
+    }
+    const params = getParams(method)
+    service[name] = async function serviceMethodWithValidation (...args) {
+      const value = combineObject(params, args)
+      const normalized = Joi.attempt(value, method.schema, {abortEarly: false})
+      // Joi will normalize values
+      // for example string number '1' to 1
+      // if schema type is number
+      const newArgs = _.map(params, (param) => normalized[param])
+      return method.apply(this, newArgs)
+    }
+    service[name].params = params
+  })
+}
+
+/**
+ * Apply logger and validation decorators to the service.
+ *
+ * @export helper/buildService
+ * @param {any} service the service to wrap
+ */
+function buildService (service) {
+  decorateWithValidator(service)
+  decorateWithLogging(service)
+}
+
+/**
+ * Verify the JWT token and get the payload.
+ *
+ * @param {String} token the JWT token to verify
+ * @returns {Object} the payload decoded from the token
+ */
+function verifyJwtToken (token) {
+  let payload
+
+  try {
+    payload = jwt.verify(token, config.JWT_TOKEN_SECRET)
+  } catch (err) {
+    if (err.message === 'jwt expired') {
+      throw createError.Unauthorized('Token has been expired')
+    }
+
+    throw createError.Unauthorized('Failed to verify token')
+  }
+
+  if (!payload) {
+    throw createError.Unauthorized('Failed to decode token')
+  }
+
+  return payload
+}
+
+/**
+ * Sign the payload and get the JWT token.
+ *
+ * @param {Object} payload the payload to be sign
+ * @returns {String} the token
+ */
+function signJwtToken (payload) {
+  return jwt.sign(payload, config.JWT_TOKEN_SECRET, {expiresIn: config.JWT_TOKEN_EXPIRES_IN})
+}
+
+/**
+ * Validate the event based on the source service, type, and message.
+ *
+ * @param {Object} event the event
+ */
+function validateEvent (event) {
+  const schema = Joi.object().keys({
+    event: Joi.object().keys({
+      type: Joi
+        .string()
+        .regex(/^([a-zA-Z0-9]+\.)+[a-zA-Z0-9]+$/)
+        .error(createError.BadRequest(
+          '"type" must be a fully qualified name - dot separated string'))
+        .required(),
+      message: Joi.string().required()
+    })
+  })
+
+  const { error } = Joi.validate({event}, schema)
+  if (error) {
+    throw error
+  }
+
+  // The message should be a JSON-formatted string
+  let message
+  try {
+    message = JSON.parse(event.message)
+  } catch (err) {
+    logger.error(err)
+    throw createError.BadRequest(
+      `"message" is not a valid JSON-formatted string: ${err.message}`)
+  }
+
+  return message
+}
+
+/**
+ * Validate the event payload
+ *
+ * @param {Object} event the event payload
+ */
+function validateEventPayload (event) {
+  const schema = Joi.object().keys({
+    event: Joi.object().keys({
+      topic: Joi
+        .string()
+        .regex(/^([a-zA-Z0-9]+\.)+[a-zA-Z0-9]+$/)
+        .error(createError.BadRequest(
+          '"topic" must be a fully qualified name - dot separated string'))
+        .required(),
+      originator: Joi.string().required(),
+      timestamp: Joi.string().required(),
+      'mime-type': Joi.string().required(),
+      payload: Joi.any()
+    })
+  })
+
+  const { error } = Joi.validate({event}, schema)
+  if (error) {
+    throw error
+  }
+}
+
+function verifyTokenScope(req, scope) {
+  const isMachineToken = _.get(req, 'authUser.isMachine', false);
+  const scopes = _.get(req, 'authUser.scopes', []);
+  if (isMachineToken && !(_.indexOf(scopes, scope) >= 0)) {
+    throw createError.Unauthorized("Check your token scope.")
+  }
+}
+
+module.exports = {
+  buildService,
+  verifyJwtToken,
+  signJwtToken,
+  validateEvent,
+  validateEventPayload,
+  verifyTokenScope
+}
--- service/PlaceholdersService.js
+++ service/PlaceholdersService.js
@@ -1,15 +1,53 @@
-'use strict';
+/**
+ * Service to find email template placeholders name.
+ */
 
+const Joi = require('joi')
+const config = require('config')
+const request = require('superagent')
+const cache = require('memory-cache')
+const tcCoreLibAuth = require('tc-core-library-js').auth
+const m2m = tcCoreLibAuth.m2m(config)
 
 /**
- * Clear placeholders cache.
- * Clear the cache for placeholder validation of email topics. 
+ * Get all email template placeholders name.
  *
- * no response value expected for this operation
- **/
-exports.clearPlaceholdersCache = function() {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
+ * @returns {Array} list with email template placeholders name
+ */
+async function getAllPlaceholders (name) {
+  const cachedData = cache.get(`placeholders-${name}`)
+  if (cachedData == null) {
+    try {
+      const token = await m2m.getMachineToken(config.AUTH0_CLIENT_ID, config.AUTH0_CLIENT_SECRET)
+      const data = await request
+        .get(`${config.TC_EMAIL_SERVICE_URL}/templates/${name}`)
+        .set('accept', 'json')
+        .set('authorization', `Bearer ${token}`)
+      const parsedData = JSON.parse(data.text)
+
+      cache.put(`placeholders-${name}`, parsedData, config.TC_EMAIL_SERVICE_CACHE_PERIOD)
+
+      return parsedData
+    } catch (err) {
+      console.log(`Error generating m2m token: ${err.message}`)
+    }
+  }
+
+  return cachedData
 }
 
+getAllPlaceholders.schema = {
+  name: Joi.string().required()
+}
+
+/**
+ * Clear template placeholder cache.
+ */
+async function clearAllPlaceholders () {
+  cache.clear()
+}
+
+module.exports = {
+  getAllPlaceholders,
+  clearAllPlaceholders
+}
