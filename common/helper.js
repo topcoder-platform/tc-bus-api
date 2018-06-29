14677f42319612452682b68893fd7245910d67e9
Mithun Kamath <callmekatootie@gmail.com>
2018-06-29 14:58:53 +0530

    Add auth checks

--- common/helper.js
+++ common/helper.js
@@ -190,7 +190,7 @@ function validateEventPayload (event) {
 }
 
 function verifyTokenScope (req, scope) {
-  const isMachineToken = _.get(req, 'authUser.isMachine', false)
+  const isMachineToken = _.get(req.swagger.params, 'authUser.isMachine', false)
   const scopes = _.get(req, 'authUser.scopes', [])
   if (isMachineToken && !(_.indexOf(scopes, scope) >= 0)) {
     throw createError.Unauthorized('Check your token scope.')
