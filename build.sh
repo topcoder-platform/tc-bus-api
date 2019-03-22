ae711571fd97829a166ca31aae292033af69ff8b
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2019-03-22 12:40:38 +0530

    STS integration

--- build.sh
+++ build.sh
@@ -6,19 +6,19 @@ set -eo pipefail
 # This script expects a single argument: NODE_ENV, which must be either
 # "development" or "production".
 
-NODE_ENV=$1
+# NODE_ENV=$1
 
-ENV=$1
-AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
-AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
-AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
-AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY")
+# ENV=$1
+# AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
+# AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
+# AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
+# AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY")
 
 
-# Builds Docker image of the app.
-TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/tc-bus-api:$CIRCLE_SHA1
-
+# # Builds Docker image of the app.
+# TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/tc-bus-api:$CIRCLE_SHA1
+TAG="tc-bus-api:latest"
 docker build -t $TAG .
 
 # Copies "node_modules" from the created image, if necessary for caching.
